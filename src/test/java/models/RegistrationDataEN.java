35efaf782193118a3bed44d59838f9b284377fd2
Sergey Evseenko <evseenkosv@tut.by>
2021-01-11 15:17:13 +0300

    Added model.

---
+++ src/test/java/models/RegistrationDataEN.java
@@ -0,0 +1,12 @@
+package models;
+
+import com.google.gson.annotations.Expose;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class RegistrationDataEN {
+    @Expose
+    RegistrationData en;
+}
