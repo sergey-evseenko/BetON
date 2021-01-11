f8d9b07675468ccb1b3b6d7886a37bfd699ba53c
Sergey Evseenko <evseenkosv@tut.by>
2021-01-11 15:18:47 +0300

    Added tests "Search Registration Data"

---
+++ src/test/java/tests/SearchRegistrationDataTest.java
@@ -0,0 +1,45 @@
+package tests;
+
+import adapters.SearchRegistrationDataAdapter;
+import models.AddressForRegistration;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class SearchRegistrationDataTest {
+
+    @DataProvider(name = "Search data params")
+    public Object[][] searchDataParams() {
+        return new Object[][]{
+                //with all params
+                {100, "DE", "80636", "München", "Marlene-Dietrich-Str.", true, 0},
+                //without postalCode
+                {100, "DE", null, "München", "Marlene-Dietrich-Str.", false, 7856},
+                //without postalCode, city, street
+                {100, "DE", null, null, null, false, 1},
+                //not full postalcode
+                {100, "DE", "81", null, null, false, 7938},
+                //without city, street
+                {100, "DE", "80636", null, null, false, 7849},
+                //without without street, street
+                {100, "DE", "80636", "München", null, false, 7849},
+                //without params
+                {0, null, null, null, null, true, 0},
+        };
+    }
+
+    @Test(description = "search registration data", dataProvider = "Search data params")
+    public void searchRegistrationData(int resultCount, String countryCode, String postcode, String city, String street, Boolean isEmptyResponse, int id) {
+        AddressForRegistration[] listOfAddresses;
+        listOfAddresses = new SearchRegistrationDataAdapter().post(resultCount, countryCode, postcode, city, street);
+        if (isEmptyResponse == true) {
+            assertEquals(listOfAddresses.length, 0, "Invalid search result");
+        } else {
+            assertEquals(listOfAddresses[0].getId(), id, "Invalid search result");
+
+        }
+
+
+    }
+}
