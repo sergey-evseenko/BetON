71d17a9b16434a45df2c8eab78644654246f40ef
Sergey Evseenko <evseenkosv@tut.by>
2021-01-13 15:40:11 +0300

    Added validate field test

---
+++ src/test/java/adapters/FieldValidationAdapter.java
@@ -0,0 +1,24 @@
+package adapters;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import models.ResponseBody;
+import models.ValidateField;
+
+import static io.restassured.RestAssured.given;
+
+public class FieldValidationAdapter extends MainAdapter {
+
+    public ResponseBody[] post(ValidateField validateField) {
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .body(gson.toJson(validateField))
+                .when()
+                .post("auth/v1/users/field/validate")
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON).extract().response();
+
+        return gson.fromJson(response.asString().trim(), ResponseBody[].class);
+    }
+}
