97b18b571307d8a3527fe117aa20fb8ded34f444
Sergey Evseenko <evseenkosv@tut.by>
2021-02-20 19:19:03 +0300

    Added "Match Day" Tests

---
+++ src/test/java/adapters/MatchDayAdapter.java
@@ -0,0 +1,32 @@
+package adapters;
+
+import models.Response;
+
+import static io.restassured.RestAssured.given;
+import static org.testng.Assert.assertEquals;
+
+public class MatchDayAdapter extends MainAdapter {
+    String url = "sport-events/v1/matchday/tornament";
+
+    public Response validate(String categoryId, String langIso, int expectedStatusCode) {
+        requestSpec = given()
+                .queryParam("categoryId", categoryId)
+                .queryParam("langIso", langIso);
+        response = get(url, requestSpec, expectedStatusCode);
+        if (expectedStatusCode == 400) {
+            return gson.fromJson(response.asString().trim(), Response.class);
+        } else {
+            assertEquals(response.asString().trim(), "[]", "Invalid response");
+            return null;
+        }
+    }
+
+    public void get(String langIso) {
+        requestSpec = given()
+                .queryParam("categoryId", "7")
+                .queryParam("langIso", langIso);
+        response = get(url, requestSpec, 200);
+        assertEquals(response.path("nm[0]"), "Utr Pro Tennis Series France, Women", "Invali response");
+    }
+
+}
---
+++ src/test/java/tests/sportEvents/MatchDayDetermination.java
@@ -0,0 +1,47 @@
+package tests.sportEvents;
+
+import adapters.MatchDayAdapter;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import tests.BaseTest;
+
+import static org.testng.Assert.assertEquals;
+
+public class MatchDayDetermination extends BaseTest {
+
+    @DataProvider(name = "Params")
+    public Object[][] params() {
+        return new Object[][]{
+                //all params are null
+                {null, null, 400, "Required String parameter 'langIso' is not present"},
+                //langIso is null
+                {"7", null, 400, "Required String parameter 'langIso' is not present"},
+                //categoryId is null
+                {null, "en", 400, "Required Long parameter 'categoryId' is not present"},
+                //invalid type of categoryId
+                {"qwerty", "en", 400, "Type mismatch."},
+                //unknow categoryId
+                {"123456789", "en", 200, ""},
+        };
+    }
+
+    @Test(description = "translate match", dataProvider = "Params")
+    public void validateParams(String categoryId, String langIso, int expectedStatusCode, String responseMessage) {
+
+        response = new MatchDayAdapter().validate(categoryId, langIso, expectedStatusCode);
+        if (expectedStatusCode == 400) {
+            assertEquals(response.getMessage(), responseMessage, "Invalid response");
+        }
+
+    }
+
+    @Test(description = "translate match")
+    public void translateMatch() {
+        new MatchDayAdapter().get("de");
+    }
+
+    @Test(description = "translate match with invalid langIso")
+    public void translateMatchInvaldLangIso() {
+        new MatchDayAdapter().get("qwerty");
+    }
+}
