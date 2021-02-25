17a4a0870d5b8cef5a179b3148f9a7dd1c6d3c36
Sergey Evseenko <evseenkosv@tut.by>
2021-02-25 23:04:40 +0300

    Removed token

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -4,7 +4,6 @@ import adapters.*;
 import com.github.javafaker.Faker;
 import models.ResponseBetOn;
 import models.User;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Listeners;
 import utils.DataReader;
 import utils.TestListener;
@@ -18,7 +17,6 @@ public class BaseTest {
     protected Faker faker = new Faker();
     protected Random random = new Random();
     protected DataReader data = new DataReader();
-    public static String token;
     protected AuthorizationAdapter authorizationAdapter = new AuthorizationAdapter();
     protected ContentAdapter contentAdapter = new ContentAdapter();
     protected GetRegistrationDataAdapted getRegistrationDataAdapted = new GetRegistrationDataAdapted();
@@ -32,11 +30,5 @@ public class BaseTest {
     protected UserAdapter userAdapter = new UserAdapter();
     protected FavouritesAdapter favouritesAdapter = new FavouritesAdapter();
     protected CatalogAdapter catalogAdapter = new CatalogAdapter();
-
-    @BeforeSuite
-    public void getToken() {
-        user = data.get("userForLogin.json", User.class);
-        token = new MainAdapter().getToken(user);
-    }
 }
 
